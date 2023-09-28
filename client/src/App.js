0fd2bde226d0c73f37a7552718e0c3d35f856ed6
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2023-09-28 05:47:24 -0500

    fixing redux state

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,35 @@ import Landing from "./scenes/Landing";
 import Projects from "./scenes/Projects";
 import NavBar from "./scenes/NavBar";
 
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blogpost/:slug" element={<BlogPost />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blogpost/:slug" element={<BlogPost />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- client/src/scenes/NavBar.jsx
+++ client/src/scenes/NavBar.jsx
@@ -1,32 +1,9 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-export default function NavBar() {
-  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
-
+export default function Projects() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <Link to="/" className="navbar-brand">
-              Home
-            </Link>
-            <Link to="/about" className="nav-item nav-link">
-              About
-            </Link>
-            <Link to="/projects" className="nav-item nav-link">
-              Projects
-            </Link>
-            <Link to="/blog" className="nav-item nav-link">
-              Blog
-            </Link>
-            <Link to="/contact" className="nav-item nav-link">
-              Contact
-            </Link>
-          </ul>
-        </div>
-      </nav>
+      <h1>Navbar goes here!</h1>
     </div>
   );
 }
