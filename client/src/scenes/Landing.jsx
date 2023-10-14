a0a08ce81e520a5801d1bf3148a26facc8b238f6
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2023-10-14 15:13:07 -0500

    Editing the landing page. Begin working on mobile version

--- client/src/scenes/Landing.jsx
+++ client/src/scenes/Landing.jsx
@@ -4,170 +4,218 @@ import SocialLinks from "../components/socialLinks";
 
 const Landing = () => {
   const theme = useTheme();
-
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const navbarHeight = 80;
 
-  return (
+  if (isNonMobileScreens) {
     // PARENT BOX
-    <Box
-      display="flex"
-      justifyContent="center"
-      flexDirection={isNonMobileScreens ? "row" : "column"}
-      alignItems="center"
-      sx={{
-        backgroundImage: `url(${Narnia})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: `calc(100vh - ${navbarHeight}px)`,
-      }}>
-      {/* LEFT BOX */}
+    return (
       <Box
-        // width="50%"
-        width={isNonMobileScreens ? "50%" : "100%"}
-        backgroundColor="rgba(0,0,0,0.5)"
-        padding={isNonMobileScreens ? "10rem 3.5rem" : "0"}
-        height="100%"
-        // margin="1rem"
-        textAlign={isNonMobileScreens ? "" : "center"}>
-        <Box padding="1rem" textAlign={isNonMobileScreens ? "" : "center"}>
-          <Typography
-            fontFamily="League Spartan"
-            fontSize={
-              isNonMobileScreens
-                ? "clamp(3rem, 4rem, 5rem)"
-                : "clamp(2rem, 3rem, 4rem)"
-            }
-            style={
-              isNonMobileScreens
-                ? { opacity: 0.5, zIndex: 4 }
-                : { opacity: 0.5 }
-            }
-            lineHeight={isNonMobileScreens ? "1" : "1.5"}
-            color={theme.palette.primary.light}>
-            Mark R
-          </Typography>
-          <Typography
-            fontFamily="League Spartan"
-            fontSize={
-              isNonMobileScreens
-                ? "clamp(5rem, 7rem, 15rem)"
-                : "clamp(2rem, 5rem, 7rem)"
-            }
-            lineHeight="1"
-            // color="#C6F6D5"
-            color={theme.palette.primary.main}
-            sx={
-              isNonMobileScreens
-                ? { position: "relative", top: "-1rem", left: "0rem" }
-                : { position: "relative", top: "-1.5rem", left: "0rem" }
-            }>
-            DeKraker
-          </Typography>
-          <Typography
-            fontWeight="mainer"
-            fontStyle="italic"
-            color={theme.palette.primary.light}
-            fontSize="1rem"
-            style={
-              isNonMobileScreens
-                ? { Top: "1rem" }
-                : { Top: "0", position: "flex", top: "" }
-            }>
-            Worshipper | Husband | Father | Full Stack Developer | Musician |
-            Athlete
-          </Typography>
-          <Typography
-            color={theme.palette.primary.light}
-            fontStyle="italic"
-            fontSize="1rem"
-            display="flex"
-            marginTop="20rem"
-            style={
-              isNonMobileScreens
-                ? {}
-                : { justifyContent: "center", marginTop: "2rem" }
-            }>
-            Find Me Here: <SocialLinks color={theme.palette.primary.main} />
-          </Typography>
-        </Box>
-      </Box>
-
-      {/* RIGHT BOX */}
-      <Box
-        width={isNonMobileScreens ? "50%" : "100%"}
-        height="100%"
         display="flex"
-        flexDirection="column"
         justifyContent="center"
+        flexDirection="row"
         alignItems="center"
-        textAlign="center"
-        padding="1rem"
-        backgroundColor="rgba(0,0,0,0.5)"
-        border="1px solid white"
-        // width="50%"
-        // border="2px solid purple"
-        // height="100%"
-        // textAlign={isNonMobileScreens ? "" : "center"}
-        // overflowY="scroll"
-      >
+        sx={{
+          backgroundImage: `url(${Narnia})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: `calc(100vh - ${navbarHeight}px)`,
+          zIndex: -11,
+        }}>
+        {/* LEFT BOX */}
         <Box
-          // border="1px solid white"
-          padding="1rem"
-          // backgroundColor="rgba(0,0,0,0.5)"
-          color={theme.palette.primary.light}
-          textAlign={isNonMobileScreens ? "" : "center"}
-          // overflowY="hidden"
-        >
+          width="50%"
+          backgroundColor="rgba(0,0,0,0.3)"
+          padding="10rem 3.5rem"
+          height="100%">
+          <Box padding="1rem" textAlign="center">
+            <Typography
+              fontFamily="League Spartan"
+              fontSize="clamp(3rem, 4rem, 5rem)"
+              style={{ opacity: 0.5 }}
+              lineHeight={1}
+              color={theme.palette.primary.light}
+              textAlign="right">
+              Mark R
+            </Typography>
+            <Typography
+              fontFamily="League Spartan"
+              fontSize="clamp(5rem, 7rem, 15rem)"
+              lineHeight="1"
+              color={theme.palette.primary.main}
+              position="relative"
+              textAlign="right">
+              DeKraker
+            </Typography>
+            <Typography
+              fontStyle="italic"
+              fontSize="clamp(0.8rem, 0.9rem, 1.5rem)"
+              lineHeight="1"
+              color={theme.palette.primary.light}
+              position="relative"
+              textAlign="right">
+              Worshipper | Husband | Father | Full Stack Developer | Musician |
+              Athlete
+            </Typography>
+            <Typography
+              color={theme.palette.primary.light}
+              fontStyle="italic"
+              fontSize="clamp(0.8rem, 1rem, 1.5rem)"
+              display="flex"
+              marginTop="20rem"
+              justifyContent="right">
+              Find Me Here: <SocialLinks color={theme.palette.primary.main} />
+            </Typography>
+          </Box>
+        </Box>
+
+        {/* RIGHT BOX */}
+        <Box
+          width="50%"
+          backgroundColor="rgba(0,0,0,0.3)"
+          padding="10rem 3.5rem"
+          height="100%">
           <Paper
-            elevation={3}
+            elevation={12}
             sx={{
+              padding: "1rem",
               color: theme.palette.primary.light,
-              maxHeight: "400px",
+              maxHeight: "calc(100vh - 80px - 20rem)", // 80px is the navbar height, 20rem is the 'Find Me Here' height
               overflowY: "scroll",
-              backgroundColor: "rgba(0,0,0,0.5)"
+              backgroundColor: "rgba(0,0,0,0.1)",
+              "&::-webkit-scrollbar": {
+                display: "none",
+              },
             }}>
-            <Typography>About</Typography>
-            <p>
+            <Typography fontSize="1.8rem" fontStyle="italic">
+              About
+            </Typography>
+            <Typography marginBottom="1rem" fontSize="0.9rem">
               In 2008 I was introduced to the world of web development. Due to
               life circumstances, I set aside my web development studies, worked
-              hard, got my degree from Liberty University Online, and then
-              became an elementary school teacher. Fast forward to 2023, I
-              completed a full stack web development bootcamp through Rice
-              University.
-            </p>
-            <p>
+              hard, got my degree in Web Development from Liberty University
+              Online, and then became an elementary school teacher. Fast forward
+              to 2023, I completed a full stack web development bootcamp through
+              Rice University.
+            </Typography>
+            <Typography marginBottom="1rem" fontSize="0.9rem">
               My main focus now is to find a job as a web developer, preferably
               focusing on front-end. In my free time, I spend time on side
               projects and learning new technologies.
-            </p>
-            <p>
+            </Typography>
+            <Typography marginBottom="1rem" fontSize="0.9rem">
               When I'm not at the computer, I am with my family, cooking, or
               working out.
-            </p>
-            <Typography>Experience</Typography>
-            <p>
-              In 2008 I was introduced to the world of web development. Due to
-              life circumstances, I set aside my web development studies, worked
-              hard, got my degree from Liberty University Online, and then
-              became an elementary school teacher. Fast forward to 2023, I
-              completed a full stack web development bootcamp through Rice
-              University.
-            </p>
-            <p>
-              My main focus now is to find a job as a web developer, preferably
-              focusing on front-end. In my free time, I spend time on side
-              projects and learning new technologies.
-            </p>
-            <p>
-              When I'm not at the computer, I am with my family, cooking, or
-              working out.
-            </p>
+            </Typography>
+            <Typography fontSize="1.8rem" fontStyle="italic">
+              Relevant Project Experience
+            </Typography>
+            <Typography marginBottom="1rem" fontSize="0.9rem">
+              My experience is largely educational and various educational
+              projects. I am trained in the MERN Full Stack. I have experience
+              with React, Node, Express, MongoDB, MySQL, and more. I am also
+              familiar with the MUI, Tailwind, & Bootstrap frameworks.
+            </Typography>
+            <Typography marginBottom="1rem" fontSize="0.9rem">
+              To view all my projects, please visit my projects page.
+            </Typography>
+            <Typography
+              fontSize="1.8rem"
+              fontStyle="italic"
+              sx={{
+                "&:hover": {
+                  textDecoration: "underline",
+                  color: theme.palette.primary.light,
+                },
+              }}>
+              <a
+                href="http://www.github.com/RDCERP/my-stores"
+                style={{
+                  textDecoration: "none",
+                  color: theme.palette.primary.light,
+                }}>
+                MyStores | github.com/RDCERP/my-stores
+              </a>
+            </Typography>
+            <Typography marginBottom="1rem" fontSize="0.9rem">
+              <ol style={{ listStyleType: "disc", paddingLeft: "1.5rem" }}>
+                <li>
+                  This project is a social media site for those who would want
+                  to share yelp locations both to ask for recommendations, or
+                  share where they've been for others to try.
+                </li>
+                <li>
+                  I provided a lot of the back end, and designed a large portion
+                  of the front end.
+                </li>
+                <li>
+                  We used JavaScript, React, Redux/Toolkit, MaterialUI, MongoDB,
+                  Express, Node, and more.
+                </li>
+              </ol>
+            </Typography>
+            <Typography
+              fontSize="1.8rem"
+              fontStyle="italic"
+              sx={{
+                "&:hover": {
+                  textDecoration: "underline",
+                  color: theme.palette.primary.light,
+                },
+              }}>
+              <a
+                href="http://github.com/Steezy1416/round-table"
+                style={{
+                  textDecoration: "none",
+                  color: theme.palette.primary.light,
+                }}>
+                RoundTable | github.com/Steezy1416/round-table
+              </a>
+            </Typography>
+            <Typography marginBottom="1rem" fontSize="0.9rem">
+              <ol style={{ listStyleType: "disc", paddingLeft: "1.5rem" }}>
+                <li>
+                  This project is a chat application utilizing Socket.io.</li>
+                <li>
+                  I provided code for the front end.
+                </li>
+                <li>
+                  We used JavaScript, Handlebars, Socket.io, Session, Sequelizer, and Express
+                </li>
+              </ol>
+            </Typography>
           </Paper>
         </Box>
       </Box>
-    </Box>
-  );
+    );
+  } else {
+    // PARENT BOX
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        alignItems="center"
+        sx={{
+          backgroundImage: `url(${Narnia})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: `calc(100vh - ${navbarHeight}px)`,
+        }}>
+        {/* Mobile Box */}
+        <Box
+        >
+          <Paper
+            elevation={2}
+          >
+            <Typography>
+              This is the mobile version of the landing page.
+            </Typography>
+          </Paper>
+        </Box>
+      </Box>
+    );
+  }
 };
 
 export default Landing;
