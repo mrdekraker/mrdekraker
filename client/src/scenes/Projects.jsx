1f689a66f46a4ceabcd8d67df0e565054e77aaba
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2023-10-23 05:28:20 -0500

    small edits

--- client/src/scenes/Projects.jsx
+++ client/src/scenes/Projects.jsx
@@ -83,7 +83,8 @@ export default function Projects() {
               <Grid item xs={3} key={project.image.asset._id}>
                 <Card
                   sx={{
-                    // maxWidth: 345,
+                    maxWidth: 345,
+                    minHeight: 345,
                     boxShadow:
                       "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
                   }}
