e6398cc9e864dae73a7182c732138bb3719b5587
Mark DeKraker <mrdekraker@MacBookPro.attlocal.net>
2025-08-02 17:47:03 -0500

    scrapping again.

--- src/app/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
-      <p>&copy; {new Date().getFullYear()} My Site. All rights reserved.</p>
-    </footer>
-  );
-}
